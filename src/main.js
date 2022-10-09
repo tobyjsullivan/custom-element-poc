1160564f804084b5e2568282eb772d9457166694
Toby Sullivan <tobyjsullivan@gmail.com>
2022-10-09 10:43:33 -0700

    Use mutation observers to detect style changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,62 @@
+const { useEffect, useState } = React;
+
 const ORIENTATION_VERTICAL = "vertical";
 const ORIENTATION_HORIZONTAL = "horizontal";
 
 const PROP_ORIENTATION = "--list-orientation";
+
+/**
+ * MutationObservers are used to monitor changes to each DOM (including both Document and shadow DOMs).
+ * A global set of observers is used to avoid creating an observer per custom element instance which could
+ * lead to a combinatorial explosion of events on each change.
+ * This design still holds some risk as we end up with a single listener callback per custom element. The
+ * hope is the callback overhead will be kept minimal by:
+ * 1. Callbacks are only fired for changes to the element's on subtree.
+ * 2. Keeping callbacks very cheap in the normal case (ie, when a mutation does not effect the element)
+ */
+const observers = {};
+const listenerGroups = {};
+
+const initObserver = (root) => {
+  if (root in observers) {
+    return;
+  }
+
+  const observer = new MutationObserver(getGroupNotifier(root));
+  observer.observe(root, {
+    subtree: true,
+    childList: true,
+    attributes: true,
+  });
+  observers[root] = observer;
+};
+
+const getListenerGroup = (root) => {
+  if (root in listenerGroups) {
+    return listenerGroups[root];
+  }
+
+  const group = [];
+  listenerGroups[root] = group;
+  return group;
+};
+
+const getGroupNotifier = (root) => () => {
+  const group = getListenerGroup(root);
+  for (const callback of group) {
+    callback();
+  }
+};
+
+const addListener = (root, callback) => {
+  const group = getListenerGroup(root);
+  group.push(callback);
+};
+
+const observe = (root, callback) => {
+  initObserver(root);
+  addListener(root, callback);
+};
 
 function CustomListInner({ orientation }) {
   if (![ORIENTATION_VERTICAL, ORIENTATION_HORIZONTAL].includes(orientation)) {
@@ -44,6 +99,10 @@ class CustomList extends HTMLElement {
     this.styles = window.getComputedStyle(this);
     this.reactRoot = ReactDOM.createRoot(shadowRoot);
 
+    // Attach listener
+    const treeRoot = this.getRootNode();
+    observe(treeRoot, () => this.update());
+
     this.update();
   }
 
@@ -65,29 +124,28 @@ class CustomList extends HTMLElement {
   }
 }
 
-function Hello({ name }) {
-  return React.createElement("h1", null, `Hello, ${name}!`);
-}
+function DemoList({ vertical }) {
+  let className = "horizontal-list";
+  if (vertical) {
+    className = "vertical-list";
+  }
 
-function VerticalList() {
   const list = React.createElement("custom-list", null, null);
 
-  return React.createElement("div", { className: "vertical-list" }, list);
+  return React.createElement("div", { className }, list);
 }
 
-function HorizontalList() {
-  const list = React.createElement("custom-list", null, null);
+function DemoContainer() {
+  const [tick, setTick] = useState(true);
 
-  return React.createElement("div", { className: "horizontal-list" }, list);
-}
+  // Tick-tocks every second
+  useEffect(() => {
+    setTimeout(() => setTick(!tick), 1000);
+  }, [tick]);
 
-function Lists() {
-  const children = [
-    React.createElement(VerticalList, { key: "vertical-example" }, null),
-    React.createElement(HorizontalList, { key: "horizontal-example" }, null),
-  ];
+  const list = React.createElement(DemoList, { vertical: tick }, null);
 
-  return React.createElement("div", { className: "lists-container" }, children);
+  return React.createElement("div", null, list);
 }
 
 function main() {
@@ -95,7 +153,7 @@ function main() {
 
   const $root = document.getElementById("root");
   const root = ReactDOM.createRoot($root);
-  root.render(React.createElement(Lists, null, null));
+  root.render(React.createElement(DemoContainer, null, null));
 }
 
 main();
